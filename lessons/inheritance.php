0eb5ed84f942397ad5f359fd73328e526b85441a
Tyler Underwood <tylergrantunderwood@gmail.com>
2022-06-27 12:39:34 -0500

    feat: getting started with inheritance

---
+++ lessons/inheritance.php
@@ -0,0 +1,20 @@
+<?php
+
+class CoffeeMaker
+{
+    public function brew()
+    {
+        var_dump('Brewing the coffee');
+    }
+}
+
+class SpecialtyCoffeeMaker extends CoffeeMaker
+{
+    public function brewLatte()
+    {
+        var_dump('Brewing the latte');
+    }
+}
+
+(new SpecialtyCoffeeMaker())->brew();
+(new SpecialtyCoffeeMaker())->brewLatte();
