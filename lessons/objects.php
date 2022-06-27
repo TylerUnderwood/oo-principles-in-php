070e397f5b132eaf6aa397760bb705e9572e25bb
Tyler Underwood <tylergrantunderwood@gmail.com>
2022-06-27 11:11:15 -0500

    feat: more constructor

--- lessons/objects.php
+++ lessons/objects.php
@@ -3,16 +3,40 @@
 class Team
 {
     protected $name;
+    protected $members = [];
 
-    public function __construct($name)
+    public function __construct($name, $members = [])
     {
         $this->name = $name;
+        $this->members = $members;
     }
 
     public function name()
+    {
+        return $this->name;
+    }
+
+    public function members()
+    {
+        return $this->members;
+    }
+
+    public function add($name)
+    {
+        $this->members[] = $name;
+    }
+
+    public function remove()
     {
 
     }
 }
 
-$acme = new Team('Acme');
+$acme = new Team('Acme', [
+    'John Doe',
+    'Jane Doe'
+]);
+
+$acme->add('Frank Doe');
+
+var_dump($acme->members());
